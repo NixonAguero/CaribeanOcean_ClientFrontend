41b2b747aae9c9dfa06ada986496693e02f75b48
Ismas28 <ismasanloa@gmail.com>
2026-04-06 16:50:51 -0600

    Refactor: Implementation of scroll function and corrections in responsive matters of rooms page

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,13 +1,18 @@
 import AboutUs from "../about-us/components/AboutUs";
 import Hero from "../hero/components/Hero";
 import { Footer } from "../../app/layout/Footer/Footer"
+import {Gallery} from "../about-us/components/Gallery";
+import { Facilities } from "../facilities/components/Facilities";
+import Locations from "../locations/components/Locations";
 
 export const HomePage = () => {
   return (
     <main>
-      <Hero />
-      <AboutUs />
-      <Footer></Footer>
+      <div id="home"> <Hero /> </div>
+      <div id="about"> <AboutUs /> </div>
+      <div id="gallery"> <Gallery /> </div>
+      <div id="facilities"> <Facilities /> </div>
+      <div id="location"> <Locations /> </div>
     </main>
   );
 };
