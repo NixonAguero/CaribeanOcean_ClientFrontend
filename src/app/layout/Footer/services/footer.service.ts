8d423402981f645cd4a7db32edc170762672eebd
Dereck Gutiérrez Cordero <148293894+DereckGC@users.noreply.github.com>
2026-04-21 19:50:24 -0600

    Refact: footer services was connected with backend to bring the hotel contact information

--- src/app/layout/Footer/services/footer.service.ts
+++ src/app/layout/Footer/services/footer.service.ts
@@ -1,27 +1,17 @@
-//import apiClient from "../../../shared/services/apiClient";
+import apiClient from "../../../../shared/services/apiClient";
 import type { FooterBrand, HotelContact, HotelLocation, HotelContactInfo} from "../types/footer.types";
 
 export async function getHotelContact(): Promise<HotelContactInfo>{
 
-    //const aboutUs = await apiClient.get("/about-us");
-
-    const hotelContact : HotelContact[] = [
-        {type : "email", contact: "info@caribbean.cr"},
-        {type : "email", contact: "reservas@caribbean.cr"},
-        {type : "whatsapp", contact: "+506 8888-0001"},
-        {type : "calls", contact: "+506 2222-1010"},
-        {type : "facebook", contact: "https://www.facebook.com/caribbeanoceantours"},
-        {type : "instagram", contact: "https://www.instagram.com/caribbean_ocean_tours/"},
-        {type : "tiktok", contact: "https://www.tiktok.com/@whattodoincuracao"}
-    ]
-
+    const { data: hotelContacts } = await apiClient.get<HotelContact[]>("/ContactUs");
+    
     const hotelLocation: HotelLocation = { 
         location: "The hotel is located fifty yards from Playa Cocles in the middle of the natural beauty of Puerto Viejo de Talamanca, Limón, Costa Rica.",
         url_location: "https://maps.app.goo.gl/f4LakXLQziZfjidq8"
     }
 
-    const hotelContactInfo : HotelContactInfo = {
-        contacts : hotelContact,
+    const hotelContactInfo : HotelContactInfo = {       
+        contacts : hotelContacts,
         location : hotelLocation
     }
 
