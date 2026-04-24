d1ea9cb93a5f24b4c18a213adcf67fed2a70638e
Dereck Gutiérrez Cordero <148293894+DereckGC@users.noreply.github.com>
2026-04-24 12:05:35 -0600

    Refact: footer service was connected with backend

--- src/features/locations/services/locations.service.ts
+++ src/features/locations/services/locations.service.ts
@@ -4,7 +4,6 @@ import type { Location } from "../types/location.types";
 
 export async function getLocation(): Promise<Location> {
   const response = await apiClient.get<Location>("/Locations");
-  console.log(response);
   return response.data;
 }
 
